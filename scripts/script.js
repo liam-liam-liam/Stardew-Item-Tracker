2960e761482f0b052549bd9c3241203919f7fbdd
liam-liam-liam <liam.kole.nordberg@gmail.com>
2026-03-10 17:37:50 -0500

    background is now random and fits screen

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,5 @@ num = Math.floor(Math.random() * 24);
 
 imageName ="frame_" +  num + "_delay-1.2s.png"
 console.log(imageName)
+
+document.getElementById("backgroundImage").style.backgroundImage = "url('assets/backgroundImages/" + imageName + "')"
